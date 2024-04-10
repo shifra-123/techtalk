db837593b2486f24e78117db2377d93ef1f781e4
Shifra Schectman <shifraschectman@TNSMAC01.attlocal.net>
2024-04-10 17:58:25 -0400

    To Do List Outline

---
+++ ToDo/ToDo.js
@@ -0,0 +1,7 @@
+/*
+    Finally, let's make our to-do list work. 
+    JavaScript allows us to add interactivity to our webpage. 
+    We will write a script to add a new task 
+    to the list when the user types a task 
+    and clicks the 'Add Task' button.
+*/
