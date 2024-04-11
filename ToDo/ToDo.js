ba13cc1f23cf2815c15c4e3179200912142ac05c
Shifra Schectman <shifraschectman@TNSMAC01.attlocal.net>
2024-04-11 11:31:48 -0400

    more changes

--- ToDo/ToDo.js
+++ ToDo/ToDo.js
@@ -4,6 +4,10 @@
     We will write a script to add a new task 
     to the list when the user types a task 
     and clicks the 'Add Task' button.
+
+    getElementById - finds an element on the page
+    addEventListener - listens for a click action. When the 
+    button is clicked, it executes the code in the  function block
 */
 document.getElementById("taskButton").addEventListener("click", function () {
   // Add what it should do on click here
